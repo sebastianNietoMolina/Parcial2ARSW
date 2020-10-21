63a9e028c45ef9cb8fee39c9da505babf731196a
sebastianNietoMolina <juan.nieto-mo@mail.escuelaing.edu.co>
2020-10-21 12:01:22 -0500

    esqueleto

---
+++ src/main/java/edu/eci/arsw/parcial/Parcial2SebastianNieto.java
@@ -0,0 +1,21 @@
+package edu.eci.arsw.parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import java.util.List;
+
+//import java.sql.Date;
+
+
+@SpringBootApplication
+public class Parcial2SebastianNieto {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcial2SebastianNieto.class, args);
+	}
+
+
+}
